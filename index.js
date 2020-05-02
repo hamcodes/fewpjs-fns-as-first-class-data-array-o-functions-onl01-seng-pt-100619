96e1b513f48429f414dbd427755ae0dadb151517
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-05-02 06:16:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let wakeDog = function(dogName="Byron", dogBreed="poodle") {
   return `Wake ${dogName} the ${dogBreed}`
 }
 
-var leashDog = function(dogName="Byron", dogBreed="poodle") {
+let leashDog = function(dogName="Byron", dogBreed="poodle") {
   console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`
 }
